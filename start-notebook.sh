2cbadfe1bfd51bcde1d3b7488cd474ebf440a632
Florian Geigl <florian.geigl@tugraz.at>
2016-11-17 16:09:06 +0100

    Update start-notebook.sh

--- start-notebook.sh
+++ start-notebook.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 mkdir -p /data/
 cd /data/
-#check for stored environments
-if [ -f environment.yml ];
-then
-  conda env create -f environment.yml
-fi
-
 exec screen -dmS ipython jupyter notebook --ip='*' --port 8888 --no-browser
