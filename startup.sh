b465b945c1623a8604db6ab5b83eef98bc683b80
Florian Geigl <florian.geigl@tugraz.at>
2016-11-17 16:08:49 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+mkdir -p /data/
+cd /data/
+#check for stored environments
+if [ -f environment.yml ];
+then
+  conda env create -f environment.yml
+fi
+
 start-notebook.sh
 start-r-server.sh
 start-ssh-server.sh
