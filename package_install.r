ffd7ac82c5f8d996e34772f5d90db08feb7f2472
Florian Geigl <florian.geigl@tugraz.at>
2016-11-18 23:26:06 +0100

    Update package_install.r

--- package_install.r
+++ package_install.r
@@ -1,9 +1,9 @@
-install.packages("data.table")
+# install.packages("data.table") # in backports
 install.packages("tools")
-install.packages("knitr")
+# install.packages("knitr") # in backports
 install.packages("RJSONIO")
 install.packages("ggmap")
-install.packages("dplyr")
+# install.packages("dplyr") # in backports
 install.packages("compiler")
 install.packages("arules")
 install.packages("arulesViz")
