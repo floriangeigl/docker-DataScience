4fb21c3db0ce51eca9d4d207dc98bee048413a41
Florian Geigl <florian.geigl@tugraz.at>
2016-11-19 01:49:53 +0100

    Update package_install.jl

--- package_install.jl
+++ package_install.jl
@@ -1,1 +1,49 @@
-Pkg.add("IJulia")
+metadata_packages = [
+    "BinDeps",
+    "Cairo",
+    "Calculus",
+    "Clustering",
+    "DataArrays",
+    "DataFrames",
+    "DataFramesMeta",
+    "Dates",
+    "DecisionTree",
+    "Distributions",
+    "Distances",
+    "Gadfly",
+    "GLM",
+    "HDF5",
+    "HypothesisTests",
+    "JSON",
+    "KernelDensity",
+    "Lora",
+    "MLBase",
+    "MultivariateStats",
+    "NMF",
+    "Optim",
+    "PDMats",
+    "RDatasets",
+    "SQLite",
+    "StatsBase",
+    "TextAnalysis",
+    "TimeSeries",
+    "ZipFile", 
+    "IJulia"]
+
+
+Pkg.init()
+Pkg.update()
+
+for package=metadata_packages
+    Pkg.add(package)
+end
+
+# need to build XGBoost version for it to work
+Pkg.clone("https://github.com/antinucleon/XGBoost.jl.git")
+Pkg.build("XGBoost")
+
+Pkg.clone("https://github.com/benhamner/MachineLearning.jl")
+Pkg.clone("https://github.com/johnmyleswhite/NearestNeighbors.jl")
+Pkg.pin("MachineLearning")
+
+Pkg.resolve()
