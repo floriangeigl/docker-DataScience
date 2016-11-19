4a3b60b170a56168b3a75409509c5f3cd4e137bf
Florian Geigl <florian.geigl@tugraz.at>
2016-11-19 01:29:41 +0100

    Update package_install.r

--- package_install.r
+++ package_install.r
@@ -1,12 +1,10 @@
 #install.packages(c('repr', 'IRdisplay', 'evaluate', 'crayon', 'pbdZMQ', 'devtools', 'uuid', 'digest'))
 #devtools::install_github('IRkernel/IRkernel')
 #IRkernel::installspec()
-# install.packages("data.table") # in backports
+install.packages("rodbc")
+install.packages("getopt")
 install.packages("tools")
-# install.packages("knitr") # in backports
-install.packages("RJSONIO")
 install.packages("ggmap")
-# install.packages("dplyr") # in backports
 install.packages("compiler")
 install.packages("arules")
 install.packages("arulesViz")
