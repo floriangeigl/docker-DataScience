f888884080b111a1c6d69ddebf0d8a5e80862c5b
Florian Geigl <florian.geigl@tugraz.at>
2016-11-30 09:03:15 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,22 +2,16 @@
 mkdir -p /data/
 cd /data/
 #check for stored environments
-if [ -f environment.yml ];
-then
+if [ -f environment.yml ]; then
   conda env create -f environment.yml
 fi
+
 echo "args: $@"
-if [[ "$@" -eq "" ]]
-  then
-    echo "No arguments supplied"
-    /usr/bin/supervisord
-else
+if [[ "$@" != "" ]]; then
   /usr/bin/supervisord >> /var/log/supervisord.log 2>&1 &
-  exec "$@"
+  exec "$@"  
+else
+  echo "No arguments supplied"
+  /usr/bin/supervisord
 fi
 
-
-#start-notebook.sh
-#start_jupyterlabs.sh
-#start-r-server.sh
-#start-ssh-server.sh
