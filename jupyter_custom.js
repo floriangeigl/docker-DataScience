bb980a21d99b3536cd3218d52ea1f10bbd807557
Florian Geigl <florian.geigl@tugraz.at>
2017-02-01 08:40:35 +0100

    Update jupyter_custom.js

--- jupyter_custom.js
+++ jupyter_custom.js
@@ -1,20 +1,3 @@
-// default init cells for new notebooks
-define([
-    'base/js/namespace'
-], function(
-    Jupyter
-) {
-    function load_ipython_extension() {
-      if (Jupyter.notebook.get_cells().length===1){
-   //do your thing
-        Jupyter.notebook.insert_cell_above('code', 0).set_text("import numpy as np\nimport pandas as pd\nimport matplotlib.pyplot as plt\nimport matplotlib as mpl\nimport seaborn as sns\nimport re");
-      }
-    }
-    return {
-        load_ipython_extension: load_ipython_extension
-    };
-});
-
 // auto-save
 define([
     'base/js/namespace',
