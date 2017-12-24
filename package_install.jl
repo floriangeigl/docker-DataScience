24602932d75693397211c9e557ed544daa5851ac
Florian Geigl <florian.geigl@gmail.com>
2017-12-24 13:11:12 +0100

    fix julia

--- package_install.jl
+++ package_install.jl
@@ -28,13 +28,15 @@ metadata_packages = [
     "TextAnalysis",
     "TimeSeries",
     "ZipFile", 
-    "IJulia"]
+    "IJulia",
+    "Plots",
+    "PyPlot"]
 
 
 Pkg.init()
 Pkg.update()
 
-for package=metadata_packages
+for package in metadata_packages
     Pkg.add(package)
 end
 
@@ -47,3 +49,7 @@ Pkg.clone("https://github.com/benhamner/MachineLearning.jl")
 Pkg.pin("MachineLearning")
 
 Pkg.resolve()
+
+Pkd.add("IJulia)
+using IJulia
+IJulia.installkernel("Julia nodeps", "--depwarn=no")
